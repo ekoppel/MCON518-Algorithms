944ed0be0ecdd109b9446b514d128b736e48608b
Ezra Koppel <ezrabkoppel@gmail.com>
2019-09-18 17:21:37 -0400

    added substring in a text with test

--- src/ContainsSubstring.java
+++ src/ContainsSubstring.java
@@ -1,2 +1,58 @@
+import java.util.HashSet;
+import java.util.Random;
+
 public class ContainsSubstring {
+    static boolean hashMethod(String text, String substring) {
+        HashSet<String> set = new HashSet<>();
+
+        for (int i = 0; i < text.length() - substring.length() + 1; i++)
+            set.add(text.substring(i, i + substring.length()));
+
+        return set.contains(substring);
+    }
+
+    static boolean walkMethod(String text, String substring) {
+        int substringIndexer = 0;
+        for (int i = 0; i < text.length(); i++) {
+            if (text.charAt(i) == substring.charAt(substringIndexer))
+                substringIndexer++;
+            else
+                substringIndexer = 0;
+
+            if (substringIndexer == substring.length())
+                return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        String t = randomString(10000);
+        String s = randomString(5);
+        System.out.println("text: " + t);
+        System.out.println("substring: " + s);
+        long start = System.nanoTime();
+        System.out.println(hashMethod(t, s));
+        long end = System.nanoTime();
+        System.out.println("hash time: " + (end - start) + " nanoseconds");
+        start = System.nanoTime();
+        System.out.println(walkMethod(t, s));
+        end = System.nanoTime();
+        System.out.println("walk time: " + (end - start) + " nanoseconds");
+    }
+
+    private static String randomString(int seed) {
+        StringBuilder sb = new StringBuilder();
+        Random rand = new Random();
+        int length = Math.abs(rand.nextInt(seed)) + 1;
+        for (int i = 0; i < length; i++)
+            sb.append(randomChar());
+        return sb.toString();
+    }
+
+    private static char randomChar(){
+        String characters = "1234567890 ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        int length = characters.length();
+        Random rand = new Random();
+        return characters.charAt(rand.nextInt(length));
+    }
 }
--- src/ContainsSubstringTest.java
+++ src/ContainsSubstringTest.java
@@ -1,5 +1,17 @@
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ContainsSubstringTest {
+    @Test
+    void SubstringTest(){
+        String s = "Hello I am a string";
+        String j = "string";
 
+        boolean doesStringExistInsideHash = ContainsSubstring.hashMethod(s, j);
+        boolean doesStringExistInsideWalk = ContainsSubstring.walkMethod(s, j);
+
+        assertTrue(doesStringExistInsideWalk);
+        assertTrue(doesStringExistInsideHash);
+    }
 }
