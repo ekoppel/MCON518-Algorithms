f8b4450eb23bf10d63d449e453d0b94c5f6cef91
Ezra Koppel <ezrabkoppel@gmail.com>
2019-12-18 16:14:29 -0500

    tweaked efficient janitor algorithm

--- src/EfficientJanitor.java
+++ src/EfficientJanitor.java
@@ -10,38 +10,46 @@ public class EfficientJanitor {
         float max = 2.02f;
         float max1 = 1.01f;
         float max2 = 1.01f;
+        boolean maxWasChanged;
 
         for (int i = 0; i < weight.size(); i++)
             if (weight.get(i) > 1.99)
                 weight.remove(i);
 
-        while (!weight.isEmpty()) {
+        while (weight.size() > 1) {
+            maxWasChanged = false;
             for (int i = 0; i < weight.size(); i++) {
                 for (int j = 0; j < weight.size(); j++) {
                     if (i == j)
                         continue;
-                    if (weight.get(i) + weight.get(j) > max) {
+                    if (weight.get(i) + weight.get(j) > max && weight.get(i) + weight.get(j) <= 3.00f) {
                         max = weight.get(i) + weight.get(j);
                         max1 = weight.get(i);
                         max2 = weight.get(j);
+                        maxWasChanged = true;
                     }
                 }
             }
-            weight.remove(max1);
-            weight.remove(max2);
-            trips--;
-            max = 2.02f;
+            if (maxWasChanged) {
+                weight.remove(max1);
+                weight.remove(max2);
+                trips--;
+                max = 2.02f;
+            }
+            else {
+                return trips;
+            }
         }
         return trips;
     }
 
-    public int solution2(List<Float> weight){
+    public int solution2(List<Float> weight) {
         float sum = 0.0f;
 
         for (Float bag : weight) {
             sum += bag;
         }
         double sum1 = (double) sum;
-        return (int) Math.ceil(sum1/3.00);
+        return (int) Math.ceil(sum1 / 3.00);
     }
 }
--- src/EfficientJanitorTest.java
+++ src/EfficientJanitorTest.java
@@ -11,19 +11,27 @@ public class EfficientJanitorTest {
     public void testJanitor(){
         EfficientJanitor janitor = new EfficientJanitor();
         List<Float> weight = new ArrayList<>();
-        weight.add(3.00f);
-        weight.add(1.20f);
-        weight.add(2.10f);
-        weight.add(1.01f);
-        weight.add(1.06f);
-        weight.add(1.78f);
-        weight.add(1.45f);
         weight.add(1.92f);
-        weight.add(1.27f);
-        weight.add(1.72f);
-        weight.add(2.72f);
-        weight.add(1.26f);
+        weight.add(1.01f);
+
+        weight.add(1.06f);
         weight.add(1.84f);
+
+        weight.add(1.51f);
+        weight.add(1.45f);
+
+        weight.add(1.51f);
+        weight.add(1.26f);
+
+        weight.add(1.72f);
+        weight.add(1.27f);
+
+        weight.add(2.10f);
+
+        weight.add(2.72f);
+
+        weight.add(1.78f);
+
         weight.add(1.28f);
         weight.add(1.15f);
 
@@ -36,24 +44,32 @@ public class EfficientJanitorTest {
     public void janitor2(){
         EfficientJanitor janitor2 = new EfficientJanitor();
         List<Float> weight = new ArrayList<>();
-        weight.add(3.00f);
-        weight.add(1.20f);
-        weight.add(2.10f);
-        weight.add(1.01f);
-        weight.add(1.06f);
-        weight.add(1.78f);
-        weight.add(1.45f);
         weight.add(1.92f);
-        weight.add(1.27f);
-        weight.add(1.72f);
-        weight.add(2.72f);
-        weight.add(1.26f);
+        weight.add(1.01f);
+
+        weight.add(1.06f);
         weight.add(1.84f);
-        weight.add(1.28f);
+
+        weight.add(1.51f);
+        weight.add(1.45f);
+
+        weight.add(1.51f);
+        weight.add(1.26f);
+
+        weight.add(1.72f);
+        weight.add(1.27f);
+
+        weight.add(2.10f);
+
+        weight.add(2.72f);
+
+        weight.add(1.78f);
         weight.add(1.15f);
+
+        weight.add(1.28f);
 
         int trips = janitor2.solution2(weight);
 
-        assertEquals(9, trips);
+        assertEquals(8, trips);
     }
 }
